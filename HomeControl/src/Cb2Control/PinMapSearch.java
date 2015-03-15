7bc559c6442e2c8b3684ca18f208ccdb6dd998ca
Tiagop <Tiagop@Tiago-PC>
2015-03-15 13:05:29 -0300

    Adionado classe contendo mapa de pinos da CB2(ainda não finalizado)

---
+++ HomeControl/src/Cb2Control/PinMapSearch.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cb2Control;
+
+/**
+ *
+ * @author Tiagop
+ */
+public class PinMapSearch {
+    /**
+     * 
+     * array contendo informaçoes sobre os pinos
+     */
+    String pinsHDMI[][]= {
+        {null,"VCC-5V"},
+        {"11","ph15"},
+        {null,"CSI1"},
+        {"10","ph15"},
+        {"9" ,"pb19"},
+        {"9" ,"pb19"},
+    };
+}
+    
+
